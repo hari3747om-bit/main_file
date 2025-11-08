8c374c910ae545ed0b21282dfcb860e033a098b7
hari3747om-bit <hari3747om@gmail.com>
2025-11-08 17:58:24 +0530

    Your commit message

---
+++ Car_game.py
@@ -0,0 +1,30 @@
+play_game= input('Do you want to play the game? Yes or no').lower()
+count_start=0
+count_stop=0
+if play_game=='yes':
+    while  True:
+        initiate=input('>').lower()
+        if initiate=='start':
+            count_start+=1
+            count_stop=0
+            if count_start>1:
+                print('The car is already started(Kya kar raha h bahinchod)')
+            elif count_start==1:
+                print('The car is started')
+        elif initiate=='stop':
+            count_stop+=1
+            count_start=0
+            if count_stop>1:
+                print('The car is already stopped(kya chutiya h re gawar)')
+            elif count_stop==1:
+                print('The car is stopped')
+        elif initiate=='help':
+            print("""
+Start: to start the car
+Stop: to stop the car
+Quit: to quit the game
+                  """)
+        elif initiate=='quit':
+            break
+elif play_game=="no":
+    print('Have a nice day')
